c7b5918072d020d9557f2393ec89fed91440abad
Hedgehog2484 <hedgehogdevelop@gmail.com>
2023-09-02 17:06:44 +0300

    Added telegram to vk handler

--- services/telegram.go
+++ services/telegram.go
@@ -8,7 +8,7 @@ import (
 	"os"
 )
 
-var queue []custom_types.Message
+var tg_queue []custom_types.Message
 var client *tbot.Client
 
 func CreateTelegramBot(token string) {
@@ -23,20 +23,19 @@ func CreateTelegramBot(token string) {
 			return
 		}
 	}()
-
 	var chatID = os.Getenv("TELEGRAM_CHAT_ID")
 	for {
-		if len(queue) > 0 {
-			var m = queue[0]
+		if len(tg_queue) > 0 {
+			var m = tg_queue[0]
 			var text = fmt.Sprintf("<b>%s</b>:\n\n%s", m.Author, m.Text)
 			_, err := client.SendMessage(chatID, text, tbot.OptParseModeHTML)
 			if err != nil {
 				log.Println("Error send tg message:", err)
 			}
-			if len(queue) > 1 {
-				queue = append(queue[:1], queue[2:]...)
+			if len(tg_queue) > 1 {
+				tg_queue = append(tg_queue[:1], tg_queue[2:]...)
 			} else {
-				queue = nil
+				tg_queue = nil
 			}
 		}
 	}
@@ -44,6 +43,9 @@ func CreateTelegramBot(token string) {
 
 func testHandler(msg *tbot.Message) {
 	if msg.Chat.ID == os.Getenv("TELEGRAM_CHAT_ID") {
-		// TODO: Пересылка сообщения в вк.
+		var userFullname = msg.From.FirstName + " " + msg.From.LastName
+		var m = custom_types.Message{Text: msg.Text, Author: userFullname}
+		// var m = custom_types.Message{Text: msg.Text, Author: "idk", Attachments: nil}
+		vk_queue = append(vk_queue, m)
 	}
 }
--- services/vk.go
+++ services/vk.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"fmt"
 	"github.com/nikepan/govkbot/v2"
 	"log"
 	"mega_chat/custom_types"
@@ -8,15 +9,37 @@ import (
 	"strconv"
 )
 
+var vk_queue []custom_types.Message
+
 func CreateVkBot(token string) {
 	govkbot.HandleMessage("", messageHandler)
 
 	govkbot.SetAPI(token, "", "5.89")
 	me, _ := govkbot.API.Me()
 	log.Println(me.FullName())
-	err := govkbot.Listen(token, "", "5.89", 819438293)
-	if err != nil {
-		log.Fatalln("VK error:", err)
+
+	go func() {
+		err := govkbot.Listen(token, "", "5.89", 819438293)
+		if err != nil {
+			log.Fatalln("VK error:", err)
+			return
+		}
+	}()
+	var chatID, _ = strconv.Atoi(os.Getenv("VK_CHAT_ID"))
+	for {
+		if len(vk_queue) > 0 {
+			var m = vk_queue[0]
+			var text = fmt.Sprintf("%s:\n\n%s", m.Author, m.Text)
+			_, err := govkbot.API.SendChatMessage(chatID, text)
+			if err != nil {
+				return
+			}
+			if len(vk_queue) > 1 {
+				vk_queue = append(vk_queue[:1], vk_queue[2:]...)
+			} else {
+				vk_queue = nil
+			}
+		}
 	}
 }
 
@@ -24,13 +47,12 @@ func messageHandler(msg *govkbot.Message) (reply string) {
 	VkChatId, _ := strconv.Atoi(os.Getenv("VK_CHAT_ID"))
 	me, _ := govkbot.API.Me()
 	if msg.ChatID == VkChatId && msg.UserID != me.ID {
-		log.Println(msg.UserID)
 		var user, err = govkbot.API.User(msg.UserID)
 		if err != nil {
 			log.Fatalln("Errror get VK user:", err)
 		}
 		var message = custom_types.Message{Text: msg.Body, Author: user.FullName(), Attachments: nil}
-		queue = append(queue, message)
+		tg_queue = append(tg_queue, message)
 		err = msg.MarkAsRead()
 		if err != nil {
 			return ""
